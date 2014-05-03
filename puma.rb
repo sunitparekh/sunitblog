6ca48a523c264559c43b67cfe3881763c7c88cbc
Sunit Parekh <parekh.sunit@gmail.com>
2014-05-03 14:36:11 +0530

    starting server using puma.rb

--- puma.rb
+++ puma.rb
@@ -8,7 +8,7 @@ daemonize true
 bind "unix://#{root}/tmp/puma/socket"
 pidfile "#{root}/tmp/puma/pid"
 state_path "#{root}/tmp/puma/state"
-stdout_redirect "#{application_path}/log/sunitblog.stdout.log","#{application_path}/log/sunitblog.stderr.log", true
+stdout_redirect "#{root}/log/sunitblog.stdout.log","#{root}/log/sunitblog.stderr.log", true
 
 rackup "#{root}/config.ru"
 
