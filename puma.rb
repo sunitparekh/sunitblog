d86f87860ffda96cc2107193791abb24a5143472
Sunit Parekh <parekh.sunit@gmail.com>
2014-05-03 14:30:17 +0530

    starting server using puma.rb

--- puma.rb
+++ puma.rb
@@ -3,12 +3,15 @@
 # start puma with:
 # bundle exec puma -C puma.rb
 
-application_path = '/root/sunitblog'
-directory application_path
-environment 'production'
+root = "#{Dir.getwd}"
 daemonize true
-pidfile "#{application_path}/tmp/pids/sunitblog.pid"
-state_path "#{application_path}/tmp/pids/sunitblog.state"
-stdout_redirect "#{application_path}/log/sunitblog.stdout.log","#{application_path}/log/sunitblog.stderr.log"
-threads 0, 16
-# bind "unix://#{application_path}/tmp/sockets/sunitblog.socket"
+bind "unix://#{root}/tmp/puma/socket"
+pidfile "#{root}/tmp/puma/pid"
+state_path "#{root}/tmp/puma/state"
+stdout_redirect "#{application_path}/log/sunitblog.stdout.log","#{application_path}/log/sunitblog.stderr.log", true
+
+rackup "#{root}/config.ru"
+
+threads 4, 8
+
+activate_control_app
